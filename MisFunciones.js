ffd0561db5767dfd1474a34b6aa5a6c887428a75
Mario <mariobl90@educastur.es>
2024-01-18 12:08:29 +0100

    Cmabios Rama Codigo

---
+++ MisFunciones.js
@@ -0,0 +1,24 @@
+function cambioColor(){
+    var color = document.getElementById('body');
+    if(color.style.backgroundColor = 'red'){
+        color.style.backgroundColor == 'white';
+    }else{
+        color.style.backgroundColor = 'red';
+    }
+}
+function cambioTitulo(){ 
+    var titulo = document.getElementById('titulo');
+    if(titulo.style.color = 'blue'){
+        titulo.style.color == 'Black';
+    }else{
+        titulo.style.color = 'blue';
+    }
+}
+function cambioTitulo(){
+    var titulo = document.getElementById('titulo');
+    if(titulo.style.color = 'blue'){
+        titulo.style.color == 'Black';
+    }else{
+        titulo.style.color = 'blue';
+    }
+}
